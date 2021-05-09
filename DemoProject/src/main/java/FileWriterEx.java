508656e7dfa3488bb0b80d27e250e0a573a612e7
nithyashanmu <nithiyashanmu@gmail.com>
2021-05-09 12:39:45 -0700

    Practice project Initial commit

---
+++ DemoProject/src/main/java/FileWriterEx.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterEx {
+
+	public static void main(String[] args) throws IOException {
+		String loc=System.getProperty("user.dir")+
+				"/UsingBufferedWriter.txt";
+		String Content="Hello";
+		
+		FileWriter filewriter=new FileWriter(loc);
+		BufferedWriter write=new BufferedWriter(filewriter);
+		write.write(Content);
+		write.newLine();
+		write.write("HI");
+		write.newLine();
+		write.write("Pgm");
+		write.close();
+		readfileEx();
+		
+	}
+	
+	public static void readfileEx() throws IOException {
+		String location=System.getProperty("user.dir")+"/UsingBufferedWriter.txt";
+		
+		FileReader reader=new FileReader(location);
+		
+		BufferedReader read=new BufferedReader(reader);
+		
+		String LineFirst=read.readLine();
+		//System.out.println(LineFirst);
+		
+		while((LineFirst=read.readLine())!=null) {
+			System.out.println(LineFirst);
+			
+		}
+	}
+	
+}
